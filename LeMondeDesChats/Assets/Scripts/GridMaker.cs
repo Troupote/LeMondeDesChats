90a9c9fe7d1cb18f19271648c13ef569c8eff728
Corentin Jouan <122738788+WalpurgisTime@users.noreply.github.com>
2024-12-06 12:50:27 +0100

    Hexagon map working

--- LeMondeDesChats/Assets/Scripts/GridMaker.cs
+++ LeMondeDesChats/Assets/Scripts/GridMaker.cs
@@ -8,12 +8,19 @@ public class GridMaker : MonoBehaviour
     private Vector3 initialPos;
 
 
+    private int index  = 0;
+
 
 
     private void Start()
     {
         initialPos = new Vector3(0,0,0);
         Instantiate(prefab,initialPos,Quaternion.identity);
+
+        int totalTiles = 1 + row * (row + 1) * 3; 
+        tilesPos = new Vector3[totalTiles];
+        
+
         TilesPlacement();
     }
 
@@ -25,7 +32,8 @@ public class GridMaker : MonoBehaviour
             new Vector3(-(Mathf.Sqrt(3) / 2) ,0, 1.5f),  
             new Vector3(Mathf.Sqrt(3) / 2,0,1.5f ),
             new Vector3((Mathf.Sqrt(3) / 2) * 2,0, 0),
-            new Vector3((Mathf.Sqrt(3) / 2) ,0, -1.5f)
+            new Vector3((Mathf.Sqrt(3) / 2) ,0, -1.5f),
+            new Vector3(-Mathf.Sqrt(3) / 2, 0, -1.5f)
 
         };
         for(int i = 1; i < row+1;i++)
@@ -37,11 +45,11 @@ public class GridMaker : MonoBehaviour
             {
                 renderer.material.color = Color.Lerp(Color.red, Color.blue, (float)i / row);
             }
-            for (int j = 0;j<i*5;j++)
+            for (int j = 0;j<6;j++)
             {
                 for (int k = 0;k<i;k++)
                 {
-                    transitionPos += NeighborOffsets[j%5];
+                    transitionPos += NeighborOffsets[j%6];
                     GameObject ui = Instantiate(prefab, transitionPos, Quaternion.identity);
                     ui.name = $"{i} + {j} + {k}";
                     Renderer rendererd = ui.GetComponent<Renderer>();
@@ -51,11 +59,52 @@ public class GridMaker : MonoBehaviour
                     }
 
                 }
-
-
-
             }
+
+        }
+    }
+
+
+    void TilesRegister()
+    {
+        Vector3[] NeighborOffsets = new Vector3[]
+        {
+            new Vector3(-(Mathf.Sqrt(3) / 2) * 2,0, 0),
+            new Vector3(-(Mathf.Sqrt(3) / 2) ,0, 1.5f),
+            new Vector3(Mathf.Sqrt(3) / 2,0,1.5f ),
+            new Vector3((Mathf.Sqrt(3) / 2) * 2,0, 0),
+            new Vector3((Mathf.Sqrt(3) / 2) ,0, -1.5f),
+            new Vector3(-Mathf.Sqrt(3) / 2, 0, -1.5f)
+
+        };
+        for (int i = 1; i < row + 1; i++)
+        {
+            Vector3 transitionPos = initialPos + new Vector3(Mathf.Sqrt(3) / 2, 0, -1.5f) * i;
             
+            GameObject uil = Instantiate(prefab, transitionPos, Quaternion.identity);
+            tilesPos[index++] = transitionPos;
+            Renderer renderer = uil.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material.color = Color.Lerp(Color.red, Color.blue, (float)i / row);
+            }
+            for (int j = 0; j < 6; j++)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    transitionPos += NeighborOffsets[j % 6];
+                    GameObject ui = Instantiate(prefab, transitionPos, Quaternion.identity);
+                    tilesPos[index++] = transitionPos;
+                    ui.name = $"{i} + {j} + {k}";
+                    Renderer rendererd = ui.GetComponent<Renderer>();
+                    if (rendererd != null)
+                    {
+                        rendererd.material.color = Color.Lerp(Color.red, Color.blue, (float)i / row);
+                    }
+
+                }
+            }
+
         }
     }
 }
