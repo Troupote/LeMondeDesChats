4abdbccb7e996cd6e68f810e3ee77c6d43e98dc9
Corentin Jouan <122738788+WalpurgisTime@users.noreply.github.com>
2024-12-08 22:25:05 +0100

    + SchoolDebut

---
+++ LeMondeDesChats/Assets/Scripts/Ai/SchoolController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SchoolController : MonoBehaviour
+{
+    
+    void GetWork()
+    {
+        SelectJob();
+        GoToSchool();
+        
+    }
+    void SelectJob()
+    {
+
+    }
+    void GoToSchool()
+    {
+
+    }
+
+    void AssignJob()
+    {
+
+    }
+}
