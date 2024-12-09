37c378d6bd34e54dad66fba701643808540b7dd7
WaifuFeros <payet.pep@gmail.com>
2024-12-09 17:09:27 +0100

    # bug fix builder waiting or changing path

--- LeMondeDesChats/Assets/Scripts/Building/BuildManager.cs
+++ LeMondeDesChats/Assets/Scripts/Building/BuildManager.cs
@@ -95,7 +95,7 @@ public class BuildManager : MonoBehaviour
         if (IsBuilding
             && tile.CanBuild
             && RessourcesGlobales.IsRessourcesAvailable(Building)
-            && HasEnoughBuilder(out var builders))
+            && HasEnoughBuilder(Building, out var builders))
         {
             Instance.StartCoroutine(Coroutine_StartBuilding(tile, Building, builders));
 
@@ -104,53 +104,39 @@ public class BuildManager : MonoBehaviour
         }
     }
 
-    public static bool HasEnoughBuilder(out AiController[] builders)
+    private static IEnumerator Coroutine_StartBuilding(Tile tile, BuildingSO SO, AiController[] builders)
+    {
+        var building = Instantiate(SO.Prefab, tile.transform);
+        building.SetActive(false);
+        tile.SetBuilding(building);
+
+        var currentBuilders = new List<AiController>();
+        for (int i = 0; i < SO.Worker; i++)
+        {
+            currentBuilders.Add(builders[i]);
+            builders[i].GoBuild(tile.transform);
+        }
+
+        yield return new WaitUntil(() => currentBuilders.All(x => x.IsAtWorkDestination()));
+
+        building.SetActive(true);
+        RessourcesGlobales.UseRessources(SO);
+
+        foreach (var builder in currentBuilders)
+            builder.EndBuild();
+    }
+
+    public static bool HasEnoughBuilder(BuildingSO SO, out AiController[] builders)
     {
         Instance._builders = GameObject.FindGameObjectsWithTag(Instance._builderTag)
             .Select(x => x.GetComponent<AiController>())
             .Where(y => y.etatActuel != AiController.AiState.Travail)
             .ToArray();
 
-        //Instance._hasEnoughBuilderComputed = true;
-        
+        // yeah optimisation, bottleneck (no), performance wasted, and all that... later
         builders = Instance._builders;
-
-        Debug.Log($"HasEnoughBuilder : {builders.Length}|{Building.Worker} = {Instance._builders.Length >= Building.Worker}");
-        return Instance._builders.Length >= Building.Worker;
+        return Instance._builders.Length >= SO.Worker;
     }
 
-    public static bool HasEnoughBuilder() => HasEnoughBuilder(out AiController[] builders);
-
-    private static IEnumerator Coroutine_StartBuilding(Tile tile, BuildingSO SO, AiController[] builders)
-    {
-        var building = Instantiate(SO.Prefab, tile.transform);
-        Debug.Log("Spawn Prefab");
-        building.SetActive(false);
-        Debug.Log("Set Instance to deactivated");
-        tile.SetBuilding(building);
-
-        Debug.Log("Send GoBuild()");
-        foreach (var builder in builders)
-        {
-            builder.GoBuild(tile.transform);
-        }
-
-        yield return new WaitUntil(() =>
-        {
-            var isAtWorkDestination = builders.All(x => x.IsAtWorkDestination());
-            Debug.Log($"All isAtWorkDestination: {isAtWorkDestination}");
-            return isAtWorkDestination;
-        });
-        Debug.Log("All reached destination");
-
-        building.SetActive(true);
-        Debug.Log("Set Instance to activated");
-        RessourcesGlobales.UseRessources(SO);
-
-        Debug.Log("Set back to wander");
-        foreach (var builder in builders)
-        {
-            builder.etatActuel = AiController.AiState.Repos;
-        }
-    }
+    public static bool HasEnoughBuilder(BuildingSO SO) => HasEnoughBuilder(SO, out AiController[] builders);
 }
