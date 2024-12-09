0ab52489c0d0cdb8ab2d25b07a5ee1d9a18fe1ca
Corentin Jouan <122738788+WalpurgisTime@users.noreply.github.com>
2024-12-09 02:44:09 +0100

    + SchoolOnGoing

--- LeMondeDesChats/Assets/Scripts/Ai/SchoolController.cs
+++ LeMondeDesChats/Assets/Scripts/Ai/SchoolController.cs
@@ -3,28 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class SchoolController : MonoBehaviour
 {
     [SerializeField] private TMP_Dropdown dropDownJobs;
     [SerializeField] private AiSelector aiSelector;
-    void GetWork()
-    {
-        GoToSchool();
-        
-    }
+    private GameObject prefabToInstantiate;
+
+    [SerializeField]
+    private GameObject[] entityPrefab;
     public void SelectJob()
     {
         aiSelector.schoolText.text = GetTextBeforeColon(aiSelector.schoolText.text) + ": "+  dropDownJobs.options[dropDownJobs.value].text;
     }
-    void GoToSchool()
+    public void GoToSchool()
     {
-
+        var aiControllerSelected =  aiSelector.aiSelected.GetComponent<AiController>();
+        aiControllerSelected.SchoolState();
     }
 
-    void AssignJob()
+    public void AssignJob()
     {
+        var aiControllerSelected = aiSelector.aiSelected.GetComponent<AiController>();
+        var age = aiControllerSelected.age;
+        var pos = aiSelector.aiSelected.transform.position;
+        Destroy(aiSelector.aiSelected);
+        foreach(var entity in entityPrefab) 
+        {
+            if(entity.tag == dropDownJobs.options[dropDownJobs.value].text)
+            {
+                prefabToInstantiate = entity;
+                break;
+            }
 
+        }
+        var obj = Instantiate(prefabToInstantiate,pos, Quaternion.identity, gameObject.transform).GetComponent<NavMeshAgent>();
+        obj.GetComponent<AiController>().age = age;
     }
 
     string GetTextBeforeColon(string text)
