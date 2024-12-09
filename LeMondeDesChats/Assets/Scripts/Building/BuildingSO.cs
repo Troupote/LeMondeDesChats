ab6f1111cabc9478f7aa263df7be8368db06638a
WaifuFeros <payet.pep@gmail.com>
2024-12-09 02:18:27 +0100

    Build ressource check + Feedbacks
    tile hover, button set to not interactable when not enought ressources

--- LeMondeDesChats/Assets/Scripts/Building/BuildingSO.cs
+++ LeMondeDesChats/Assets/Scripts/Building/BuildingSO.cs
@@ -8,4 +8,6 @@ public class BuildingSO : ScriptableObject
     [field: SerializeField] public GameObject Prefab { get; private set; }
     [field: SerializeField, Min(0)] public int Wood { get; private set; }
     [field: SerializeField, Min(0)] public int Stone { get; private set; }
+
+    // Need number of builder
 }
